593ee33423353d6584932cfff1003d853136ebe0
Nestor <nestorpuentesin@gmail.com>
2023-08-17 12:56:52 -0600

    agregue dotenv y modifique env 3

--- backend/Connection.php
+++ backend/Connection.php
@@ -4,9 +4,8 @@ class Connection{
 	private $conn;
   	// private $database;
 	function __construct() {
-		echo "Connection.php-> ".__DIR__;
-		// Cargar autoload.php
-		require_once __DIR__ . '/vendor/autoload.php';
+		echo "Connection.php-> ";
+		
 		echo "se cargo el autoload";
 		// Cargar variables de entorno desde el archivo .env
 		$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
