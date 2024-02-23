0bca22e90c413b50281e31c606569196e5467d19
Nestor <nestorpuentesin@gmail.com>
2024-02-23 12:12:40 -0600

    redirecto config index login particulas

--- scripts/particles.js
+++ scripts/particles.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load("particles-js", "../assets/particlesjs.json   ", function () {
+particlesJS.load("particles-js", "../empleos/assets/particlesjs.json   ", function () {
    // console.log("callback - particles.js config loaded");
 });
