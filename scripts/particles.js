a4525420f8a9bb5ceec6346fbcc2d6719580c697
Nestor <nestorpuentesin@gmail.com>
2024-10-17 12:59:02 -0600

    se quito el autofocus 4

--- scripts/particles.js
+++ scripts/particles.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load("particles-js", "/empleos/assets/particlesjs.json   ", function () {
-   // console.log("callback - particles.js config loaded");
-});
+// particlesJS.load("particles-js", "/empleos/assets/particlesjs.json   ", function () {
+//    // console.log("callback - particles.js config loaded");
+// });
