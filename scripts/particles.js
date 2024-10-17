285abc393f3baa355d1bf535f8693df468eb8e24
Nestor <nestorpuentesin@gmail.com>
2024-10-17 14:35:35 -0600

    volvi a activar las particulas

--- scripts/particles.js
+++ scripts/particles.js
@@ -1,4 +1,6 @@
+const $URL_BASE = $("#url_base").val();
+
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-// particlesJS.load("particles-js", "/empleos/assets/particlesjs.json   ", function () {
-//    // console.log("callback - particles.js config loaded");
-// });
+particlesJS.load("particles-js", `${$URL_BASE}/assets/particlesjs.json   `, function () {
+   // console.log("callback - particles.js config loaded");
+});
