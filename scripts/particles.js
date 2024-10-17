93e4c068a7277db6ec9a18cfbeb4dcffdea2ad9e
Nestor <nestorpuentesin@gmail.com>
2024-10-17 12:55:41 -0600

    se quito el autofocus 3

--- scripts/particles.js
+++ scripts/particles.js
@@ -1,7 +1,4 @@
-const URL_BASE_ = document.getElementById("url_base").value;
-// console.log("en el archivo particles.js", URL_BASE_);
-
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load("particles-js", `${URL_BASE_}/assets/particlesjs.json`, function () {
+particlesJS.load("particles-js", "/empleos/assets/particlesjs.json   ", function () {
    // console.log("callback - particles.js config loaded");
 });
