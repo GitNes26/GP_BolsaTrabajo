b660736b5b114a6f8f4b7f8a929c0452e40ca90d
Nestor <nestorpuentesin@gmail.com>
2024-10-17 12:33:48 -0600

    se corrigio un errror fantasma de valdiateInputs en login 2

--- scripts/particles.js
+++ scripts/particles.js
@@ -1,4 +1,7 @@
+const URL_BASE_ = document.getElementById("url_base").value;
+// console.log("en el archivo particles.js", URL_BASE_);
+
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load("particles-js", `${document.getElementById("url_base").value}/assets/particlesjs.json`, function () {
+particlesJS.load("particles-js", `${URL_BASE_}/assets/particlesjs.json`, function () {
    // console.log("callback - particles.js config loaded");
 });
