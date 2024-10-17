87f4a079bebfaa6f2f7e57f24bddebba728f6652
Nestor <nestorpuentesin@gmail.com>
2024-10-17 15:26:17 -0600

    sin login 2

--- index.php
+++ index.php
@@ -102,7 +102,98 @@ if (isset($_GET["p"])) {
                <div
                   class="container-fluid d-flex justify-content-center overflow-auto py-3 flex-column align-items-center "
                   style="z-index:1">
+                  <div class="login-box">
+                     <div class="login-logo">
+                        <!-- <span class="fw-bold h1">RESO</span>
+                          <span class="fw-light h1">Sistemas</span> -->
+                        <div class="login-logo">
+                           <img src="<?= $LOGO ?>" alt='GP Logo' class='img-fluid' />
+                        </div>
+                     </div>
+                     <!-- /.login-logo -->
 
+                     <!-- Card Login -->
+                     <div class="card rounded-3 card-outline card-success shadow" id="card_login" data-slide-down="1">
+                        <div class="card-body login-card-body">
+                           <p class="login-box-msg text-sm fst-italic">Ingresa tus credenciales para inicar sesión</p>
+
+                           <form id="form_login">
+                              <input type="hidden" id="op" name="op" value="login">
+                              <div class="form-floating mb-3">
+                                 <input type="email" class="form-control rounded-lg" id='email' name='email'
+                                    placeholder="Correo Electronico" data-input-name="Correo" value="<?= $e ?>" />
+                                 <label for="email">Correo Electrónico</label>
+                              </div>
+                              <div class="form-floating mb-3">
+                                 <input type="password" class="form-control" id='password' name='password'
+                                    placeholder="Contraseña" autocomplete="off" data-input-name="CONTRASEÑA"
+                                    value="<?= $p ?>" />
+                                 <label for="password">Contraseña</label>
+                                 <i class="fa-duotone fa-eye-slash eye_icon" data-input="password"></i>
+                              </div>
+                              <div class="row">
+                                 <div class="col">
+                                    <button type="submit" id="btn_login"
+                                       class="btn btn-outline-success btn-block fw-bold text-center">
+                                       <i class="fa-solid fa-circle-arrow-right"></i>&nbsp;&nbsp;INICIAR SESIÓN
+                                    </button>
+                                    <br>
+                                    <a class="float-start" id="btn_signup" style="cursor:pointer">¡REGISTRARME!</a>
+                                    <!-- <a href="/" class="float-end">Regresar a gomezpalacio.gom.mx</a> -->
+                                 </div>
+                                 <!-- /.col -->
+                              </div>
+                           </form>
+                        </div>
+                        <!-- /.login-card-body -->
+                     </div>
+
+                     <!-- Card Regitro -->
+                     <div class="card rounded-3 card-outline card-success shadow" id="card_register">
+                        <div class="card-body login-card-body">
+                           <p class="login-box-msg text-sm fst-italic">Favor de llenar todos los campos.</p>
+
+                           <form id="form_register">
+                              <div class="form-floating mb-3">
+                                 <input type="email" class="form-control rounded-lg" id='input_email' name='input_email'
+                                    placeholder="Correo Electrónico" data-input-name=" CORREO" />
+                                 <label for="input_email">Correo Electrónico</label>
+                              </div>
+                              <div class="form-floating mb-3">
+                                 <input type="password" class="form-control" id='input_password' name='input_password'
+                                    placeholder="Contraseña" autocomplete="off" data-input-name="CONTRASEÑA" />
+                                 <label for="input_password">Contraseña</label>
+                                 <i class="fa-duotone fa-eye-slash eye_icon" tittle="show text"
+                                    data-input="input_password"></i>
+
+                              </div>
+                              <div class="form-floating mb-3">
+                                 <input type="password" class="form-control" id='input_confirm_password'
+                                    name='input_confirm_password' placeholder="Confirm Password" autocomplete="off"
+                                    data-input-name="CONFIRMAR CONTRASEÑA" />
+                                 <label for="input_confirm_password">Confirmar Contraseña</label>
+                                 <span class="fst-italic" id="feedback_confirm_password"></span>
+                                 <i class="fa-duotone fa-eye-slash eye_icon" tittle="show text"
+                                    data-input="input_confirm_password"></i>
+                              </div>
+
+                              <div class="row">
+                                 <div class="col">
+                                    <button type="submit" id="btn_register"
+                                       class="btn btn-outline-success btn-block fw-bold text-center">
+                                       <i class="fa-solid fa-circle-arrow-up"></i>&nbsp;&nbsp;REGISTRARME
+                                    </button>
+                                    <br>
+                                    <a class="float-start" id="btn_signin" style="cursor:pointer">Ya tengo cuenta</a>
+                                    <!-- <a href="/" class="float-end">Return to main page</a> -->
+                                 </div>
+                                 <!-- /.col -->
+                              </div>
+                           </form>
+                        </div>
+                        <!-- /.Registro-card-body -->
+                     </div>
+                  </div>
                   <!-- /.Registro-box -->
 
                   <!-- CONTENIDO DE PRIVACIDAD -->
